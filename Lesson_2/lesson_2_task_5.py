e0a6a46e7f122be911a78a8d0e49658ddd8b648e
Vadimkandeev <vadimkandeev@gmail.com>
2023-01-29 19:21:48 +0400

    Внесено исправление 29.01.2023

--- Lesson_2/lesson_2_task_5.py
+++ Lesson_2/lesson_2_task_5.py
@@ -1,5 +1,5 @@
 def month_to_season(month):
-    if (1 <= month <= 3) or month == 12: # Исправлено условие
+    if (1 <= month <= 3) or month == 12: # Внесено исправление
         print("Winter")
     elif 3 <= month <= 5:
         print("Spring")
