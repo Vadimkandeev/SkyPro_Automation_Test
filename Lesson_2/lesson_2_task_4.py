308e1ec9ab927f08a5fcb49fc6422f5fd6b91264
Vadimkandeev <vadimkandeev@gmail.com>
2023-01-30 00:00:23 +0400

    Вот теперь точно работает

--- Lesson_2/lesson_2_task_4.py
+++ Lesson_2/lesson_2_task_4.py
@@ -1,18 +1,13 @@
-def cycle(x): # Добавлена функция цикла, которая вызывается
-    for i in range(1, x+1):  # в условном операторе
-        print(i)
-
-
 def fizz_buz(num):
-    if (num % 3 == 0) and (num % 5 == 0):
-        cycle(num) # <<--
-        print("FizzBuzz")
-    elif num % 3 == 0:
-        cycle(num) # <<--
-        print("Fizz")
-    elif num % 5 == 0:
-        cycle(num) # <<--
-        print("Buzz")
+    for i in range(1, num + 1):
+        if (i % 3 == 0) and (i % 5 == 0):
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
 
 
 fizz_buz(int(input()))
