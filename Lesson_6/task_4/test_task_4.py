ca9ea091b9a80b61c10e3242718b34801f630218
Vadimkandeev <vadimkandeev@gmail.com>
2023-02-14 12:13:04 +0400

    Домащняя работа_6

--- Lesson_6/task_4/test_task_4.py
+++ Lesson_6/task_4/test_task_4.py
@@ -3,70 +3,70 @@ from task_4 import Registrated
 import pytest
 
 
-def browser_():
+def test_browser_():
     registrated = Registrated()
     registrated.browser()
         
 # заполняем поля  
 @pytest.mark.parametrize("locator, data", ["[name='first-name']", "Иван"])
-def fill_field_first_name(locator, data):
+def test_fill_field_first_name(locator, data):
     registrated = Registrated()
     registrated.input_first_name(locator, data)
 
 
 
 @pytest.mark.parametrize("locator, data", ["[name='last-name']", "Петров"])
-def fill_field_last_name(locator, data):
+def test_fill_field_last_name(locator, data):
     registrated = Registrated()
     registrated.input_last_name(locator, data)
     
     
 @pytest.mark.parametrize("locator, data", ["[name='address']", "Ленина, 55-3"])
-def fill_field_last_name(locator, data):
+def test_fill_field_last_name(locator, data):
     registrated = Registrated()
     registrated.input_address(locator, data)
 
 
 @pytest.mark.parametrize("locator, data", ["[name='zip-code']", "", "#zip-code"])
-def fill_field_last_name(locator, data):
+def test_fill_field_last_name(locator, data):
     registrated = Registrated()
     registrated.input_zip_code(locator, data)
 
 
 @pytest.mark.parametrize("locator, data", ["[name='city']", "Москва"])
-def fill_field_last_name(locator, data):
+def test_fill_field_last_name(locator, data):
     registrated = Registrated()
     registrated.input_city(locator, data)
 
 
 @pytest.mark.parametrize("locator, data", ["[name='country']", "Россия"])
-def fill_field_last_name(locator, data):
+def test_fill_field_last_name(locator, data):
     registrated = Registrated()
     registrated.input_country(locator, data)
 
 
 @pytest.mark.parametrize("locator, data", ["[name='job-position']", "QA"])
-def fill_field_last_name(locator, data):
+def test_fill_field_last_name(locator, data):
     registrated = Registrated()
     registrated.input_job_position(locator, data)
 
 
 @pytest.mark.parametrize("locator, data", ["[name='company']", "SkyPro"])
-def fill_field_last_name(locator, data):
+def test_fill_field_last_name(locator, data):
     registrated = Registrated()
     registrated.input_company(locator, data)
 #----------------------------------------------------------------------
 
 
 # жмем на кнопку "Применить"
-def click():
+def test_click():
     registrated = Registrated()
     registrated.button_click()
 
 
 #  Проводим проверку  цвета полей ввода
 @pytest.mark.parametrize("#first-name")    
-def color_field_first_name(locator):
+def test_color_field_first_name(locator):
     registrated = Registrated()
     f_color = registrated.check_lield_first_name(locator)
     assert f_color == "rgba(209, 231, 221, 1)"
@@ -74,21 +74,21 @@ def color_field_first_name(locator):
 
 
 @pytest.mark.parametrize("#last-name")    
-def color_field_last_name(locator):
+def test_color_field_last_name(locator):
     registrated = Registrated()
     f_color = registrated.check_field_last_name(locator)
     assert f_color == "rgba(209, 231, 221, 1)"
 
 
 @pytest.mark.parametrize("#address")    
-def color_field_address(locator):
+def test_color_field_address(locator):
     registrated = Registrated()
     f_color = registrated.check_field_address(locator)
     assert f_color == "rgba(209, 231, 221, 1)"
 
 
 @pytest.mark.xfail("#zip-code")    
-def color_field_zip_code(locator):
+def test_color_field_zip_code(locator):
     registrated = Registrated()
     f_color = registrated.check_field_zip_code(locator)
     assert f_color == "rgba(209, 231, 221, 1)"
@@ -96,35 +96,35 @@ def color_field_zip_code(locator):
 
 
 @pytest.mark.parametrize("#city")    
-def color_field_city(locator):
+def test_color_field_city(locator):
     registrated = Registrated()
     f_color = registrated.check_field_city(locator)
     assert f_color == "rgba(209, 231, 221, 1)"
 
 
 @pytest.mark.parametrize("#country")    
-def color_field_country(locator):
+def test_color_field_country(locator):
     registrated = Registrated()
     f_color = registrated.check_field_country(locator)
     assert f_color == "rgba(209, 231, 221, 1)"
 
 
 @pytest.mark.parametrize("#job-position")    
-def color_field_job_position(locator):
+def test_color_field_job_position(locator):
     registrated = Registrated()
     f_color = registrated.check_field_job_position(locator)
     assert f_color == "rgba(209, 231, 221, 1)"
 
 
 @pytest.mark.parametrize("#company")    
-def color_field_company(locator):
+def test_color_field_company(locator):
     registrated = Registrated()
     f_color = registrated.check_field_company(locator)
     assert f_color == "rgba(209, 231, 221, 1)"
 #---------------------------
     
 # Закрываем браузер    
-def browser_close():
+def test_browser_close():
     registrated = Registrated()
     registrated.quit_browser()
    
