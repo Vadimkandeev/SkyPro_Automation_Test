987c64de84848f7464b409c35bb3db0a8ed1bb49
Vadimkandeev <vadimkandeev@gmail.com>
2023-02-16 00:51:30 +0400

    111

--- Lesson_6/task_5/test_task_5.py
+++ Lesson_6/task_5/test_task_5.py
@@ -5,13 +5,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
-
+import pytest
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 driver.get('https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html')
 
 driver.maximize_window()
+
+
 
 driver.find_element(By.CSS_SELECTOR, "#delay").clear()
 driver.find_element(By.CSS_SELECTOR, "#delay").send_keys("45")
@@ -24,14 +26,14 @@ driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[2]').click()
 
 driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[15]').click()
 
-sleep(48)
-"""
+#sleep(48)
+
 waiter = WebDriverWait(driver, 48)
 
 waiter.until(
-    EC.text_to_be_present_in_element((By.CSS_SELECTOR, "[aria-valuenow]"), "75")
+    EC.text_to_be_present_in_element((By.XPATH, '[//*[@id="calculator"]/div[1]/div'), "15")
 )
-"""
+
 res = driver.find_element(By.XPATH, '//*[@id="calculator"]/div[1]/div').text 
 def test_calc():  
     assert res == "15"
--- Lesson_6/task_6/task_6.py
+++ Lesson_6/task_6/task_6.py
@@ -10,4 +10,6 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 
 driver.get('https://www.saucedemo.com/')
 
-driver.find_element(By.CSS_SELECTOR, "#user-name").send_keys("standard_user")
+driver.find_element(By.CSS_SELECTOR, "#user-name").send_keys("standard_user")  
+
+driver.find_element(By.CSS_SELECTOR, "#password").send_keys("secret_sauce")
